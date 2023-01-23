5848c9f649a83fddbf218fa41ff2b6594074690a
p16z <32718673+p16z@users.noreply.github.com>
2023-01-23 17:09:38 +0000

    change latex file

--- make-latex.sh
+++ make-latex.sh
@@ -3,7 +3,6 @@
 
 pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/pre.tex
 pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/intro.tex
-pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
 
 for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
@@ -11,6 +10,7 @@ for filename in text/ch*.txt; do
 done
 
 pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/web.tex
+pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
@@ -18,7 +18,6 @@ for filename in text/apx*.txt; do
 done
 
 pandoc -s latex/*.tex -o book_result/generated_book.tex
-
 pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="OpenSans-Regular.ttf" --variable sansfont="OpenSans-Regular.ttf" --variable monofont="OpenSans-Regular.ttf" --variable fontsize=12pt --variable version=2.0 book_result/generated_book.tex  --pdf-engine=xelatex --toc -o book_result/generated_book.pdf
 
 
